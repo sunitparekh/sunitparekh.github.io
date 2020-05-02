528a812ed30161d83ca5806cf61419db19884aab
Sunit Parekh <sunitp@thoughtworks.com>
2020-05-02 23:37:05 +0530

    v4

---
+++ images/22/structured-logging.mm
@@ -0,0 +1,90 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1400987195516" ID="ID_1119270512" MODIFIED="1400993495329" TEXT="Structured Logging">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1400993472470" ID="ID_1385645455" MODIFIED="1400994005461" POSITION="right" TEXT="What?">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1400994005462" ID="ID_697635950" MODIFIED="1400994032840" TEXT="Log messages with key=value pair">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1400993516763" ID="ID_615675801" MODIFIED="1400994073831" POSITION="right" TEXT="Why?">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1400993528468" ID="ID_614221081" MODIFIED="1400993540038" TEXT="Debugging">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1400993540579" ID="ID_167819801" MODIFIED="1400993543678" TEXT="Reporting">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1400993567507" ID="ID_1956829312" MODIFIED="1400993581882" TEXT="Monitoring &amp; alerting">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1400993544124" ID="ID_544469647" MODIFIED="1400993566814" TEXT="Performance benchmarking">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1401006498551" ID="ID_1806062528" MODIFIED="1401006513074" TEXT="Big data analytics">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1400994033404" ID="ID_638328880" MODIFIED="1401007274841" POSITION="right" TEXT="Aggregate logs">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1401006532175" ID="ID_836419768" MODIFIED="1401007259446" TEXT="File replication">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1401006540983" ID="ID_1719953860" MODIFIED="1401007259447" TEXT="Syslog">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1401006543159" ID="ID_1051535658" MODIFIED="1401007259448" TEXT="Agents">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1400993397239" ID="ID_626733802" MODIFIED="1401007288720" POSITION="right" TEXT="Tools">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1400995399400" ID="ID_356175043" MODIFIED="1400995411980" TEXT="Commercial">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1400993593771" ID="ID_699799275" LINK="http://www.splunk.com/" MODIFIED="1400995408049" TEXT="Splunk">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1400994734495" ID="ID_1550390113" MODIFIED="1400995301435" TEXT="SaaS">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1400993597314" ID="ID_1764021851" LINK="https://logentries.com/" MODIFIED="1400995322201" TEXT="LogEntries">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1400995324857" ID="ID_1576449654" LINK="https://www.loggly.com/" MODIFIED="1400995345262" TEXT="Loggly">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1400995348952" ID="ID_1190319134" MODIFIED="1400995352011" TEXT="OpenSource">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1400993606098" ID="ID_1346376970" LINK="http://www.graylog2.org/" MODIFIED="1400995355637" TEXT="GrayLog2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1400993711674" ID="ID_1444512740" LINK="http://logstash.net/" MODIFIED="1400995357951" TEXT="LogStash">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ images/24/mindmap.mm
@@ -0,0 +1,39 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1403187110306" ID="ID_1745958557" MODIFIED="1403187171200" TEXT="CD on Legacy Projects">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1403187175750" ID="ID_1258456684" MODIFIED="1403187223598" POSITION="right" TEXT="1. Automated build and deployment">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1403187187468" ID="ID_1420810411" MODIFIED="1403187237277" POSITION="right" TEXT="2. Automated sanity test suite">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1403187204075" ID="ID_1877756718" MODIFIED="1403187240925" POSITION="right" TEXT="3. Development with unit tests and refactoring">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1403187247091" ID="ID_251304217" MODIFIED="1403187253941" POSITION="right" TEXT="Test last to test first journey">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1403187257131" ID="ID_895131700" MODIFIED="1403187334315" POSITION="right" TEXT="Three levels of refactoring">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1403187265473" ID="ID_1649056769" MODIFIED="1403187271948" TEXT="Code">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403187272706" ID="ID_921299746" MODIFIED="1403187274365" TEXT="Design">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403187274890" ID="ID_61427419" MODIFIED="1403187283620" TEXT="Architecture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
